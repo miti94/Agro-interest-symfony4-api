2eaa4941fe2b3168082b5b129b009c1a876924b2
amare16 <amarenighatu16@gmail.com>
2020-12-24 20:06:46 +0100

    edit migration file

--- src/Migrations/Version20191014211417.php
+++ src/Migrations/Version20191014211417.php
@@ -20,7 +20,7 @@ final class Version20191014211417 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'pgsql');
 
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(100) NOT NULL, last_name VARCHAR(100) NOT NULL, username VARCHAR(45) NOT NULL, email VARCHAR(100) NOT NULL, password VARCHAR(100) NOT NULL, telephone VARCHAR(15) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
@@ -28,7 +28,7 @@ final class Version20191014211417 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'pgsql');
 
         $this->addSql('DROP TABLE user');
     }
