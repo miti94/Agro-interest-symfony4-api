40bb054e648cf8ebda09c315d891767967132847
amare16 <amarenighatu16@gmail.com>
2020-12-24 22:43:26 +0100

    add require vendor/autoload.php in index.php

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/config/bootstrap.php';
+require __DIR__.’/vendor/autoload.php’;
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
